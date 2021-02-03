572f19455c3cd94e8655cf037ec1b5ecce18659c
Max Pochepets <agreed_note@hotmail.com>
2021-02-03 20:15:35 +0200

    Initial

--- src/main/java/cinema/shop/dao/OrderDao.java
+++ src/main/java/cinema/shop/dao/OrderDao.java
@@ -1,12 +1,11 @@
 package cinema.shop.dao;
 
 import cinema.shop.model.Order;
-import cinema.shop.model.ShoppingCart;
 import cinema.shop.model.User;
 import java.util.List;
 
 public interface OrderDao {
-    Order completeOrder(ShoppingCart shoppingCart);
+    Order add(Order order);
 
     List<Order> getOrdersHistory(User user);
 }
