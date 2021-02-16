7d0005b381fa3b66af151ecaa737cc57e2a36a34
Max Pochepets <agreed_note@hotmail.com>
2021-02-16 17:57:31 +0200

    Refactor:
    deleted AuthenticationException.java

--- src/main/java/cinema/shop/lib/exception/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package cinema.shop.lib.exception;
-
-public class AuthenticationException extends Exception {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
