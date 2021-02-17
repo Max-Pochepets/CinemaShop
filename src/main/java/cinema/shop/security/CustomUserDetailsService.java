85c4fbfa6bc1034227323c0da73c2dd6faed8d70
Max Pochepets <agreed_note@hotmail.com>
2021-02-17 10:35:03 +0200

    Fix:
    CustomUserDetailsService exception throwing fixed

--- src/main/java/cinema/shop/security/CustomUserDetailsService.java
+++ src/main/java/cinema/shop/security/CustomUserDetailsService.java
@@ -2,7 +2,7 @@ package cinema.shop.security;
 
 import cinema.shop.model.User;
 import cinema.shop.service.UserService;
-import java.util.Optional;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,16 +18,12 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Optional<User> userByEmail = userService.findByEmail(email);
-        if (userByEmail.isEmpty()) {
-            throw new UsernameNotFoundException("Could not find user by email " + email + ". ");
-        }
-
-        org.springframework.security.core.userdetails.User.UserBuilder builder = null;
+        User user = userService.findByEmail(email).orElseThrow(()
+                -> new UsernameNotFoundException("Could not find user by email " + email + ". "));
+        UserBuilder builder = null;
         builder = org.springframework.security.core.userdetails.User.withUsername(email);
-        builder.password(userByEmail.get().getPassword());
-        builder.roles(userByEmail.get().getRole().toString());
-
+        builder.password(user.getPassword());
+        builder.roles(user.getRole().toString());
         return builder.build();
     }
 }
