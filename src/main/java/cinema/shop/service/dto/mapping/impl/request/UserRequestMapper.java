1ec95b2cbe864e8a6fceb6c637e8b5555d3ddb7a
Max Pochepets <agreed_note@hotmail.com>
2021-02-22 13:49:08 +0200

    Removed password encoding in UserRequestMapper, now it's being encoded in UserServiceImpl; Moved PasswordEncoder bean to AppConfig

--- src/main/java/cinema/shop/service/dto/mapping/impl/request/UserRequestMapper.java
+++ src/main/java/cinema/shop/service/dto/mapping/impl/request/UserRequestMapper.java
@@ -3,22 +3,15 @@ package cinema.shop.service.dto.mapping.impl.request;
 import cinema.shop.model.User;
 import cinema.shop.model.dto.request.UserRequestDto;
 import cinema.shop.service.dto.mapping.DtoRequestMapper;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserRequestMapper implements DtoRequestMapper<UserRequestDto, User> {
-    private final PasswordEncoder encoder;
-
-    public UserRequestMapper(PasswordEncoder encoder) {
-        this.encoder = encoder;
-    }
-
     @Override
     public User fromDto(UserRequestDto dto) {
         User user = new User();
         user.setEmail(dto.getEmail());
-        user.setPassword(encoder.encode(dto.getPassword()));
+        user.setPassword(dto.getPassword());
         return user;
     }
 }
--- src/main/java/cinema/shop/service/impl/UserServiceImpl.java
+++ src/main/java/cinema/shop/service/impl/UserServiceImpl.java
@@ -4,18 +4,22 @@ import cinema.shop.dao.UserDao;
 import cinema.shop.model.User;
 import cinema.shop.service.UserService;
 import java.util.Optional;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserDao userDao;
+    private final PasswordEncoder encoder;
 
-    public UserServiceImpl(UserDao userDao) {
+    public UserServiceImpl(UserDao userDao, PasswordEncoder encoder) {
         this.userDao = userDao;
+        this.encoder = encoder;
     }
 
     @Override
     public User add(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return userDao.add(user);
     }
 
